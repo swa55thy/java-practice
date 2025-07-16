46f99d173bb5803ba599b3b5988f8ce2545f9522
Swathy S <swathyswathy24610@gmail.com>
2025-07-16 22:30:05 +0530

    Add files via upload

---
+++ DataTypes.java
@@ -0,0 +1,13 @@
+public class DataTypes {
+    
+    public static void main(String[] args) {
+        int empno = 15;
+        char last ='S';
+        String name = "Robert";
+        float salary = 10000;
+        System.out.println("Name Lastname : "+name+last);
+        System.out.println("salary :"+salary);
+        System.out.println("employee no :"+empno);
+    }
+    
+}
